3cc022cf6a41f622bcff1653d5678ac0bc44bda0
chrisnnchris <chrisnnchris@gmail.com>
2018-04-15 22:01:45 -0700

    Found a way to get all elements through scroll view
    Clever web design by Google. Resource light.

---
+++ songListScrapers.js
@@ -0,0 +1,8 @@
+//look at songlist-container for google play music
+var googlePlaySongs = document.getElementsByClassName("song-table")[0].children[1];
+console.log(googlePlaySongs);
+
+/*
+var googlePlaySongs = document.getElementsByClassName("song-table")[0].children[1].children[20];
+googlePlaySongs.scrollIntoView();
+*/
