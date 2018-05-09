30063f0170040d4c1e9fdd9955b4f2b6c063b11d
chrisnnchris <chrisnnchris@gmail.com>
2018-05-08 18:57:33 -0700

    Got the CSV export
    Did not export right though, no spaces. However, categories preserved.

--- songListScrapers.js
+++ songListScrapers.js
@@ -4,7 +4,7 @@ var songTableLength = googlePlaySongs.getAttribute("data-count");
 console.log(googlePlaySongs);
 console.log("This is songTableLength " + songTableLength); // works, but NOT TESTED ABOVE 50 songs
 var songScrapArray = [];
-
+songScrapArray.push("Song Title,Artist Title,Album Title");
 songTableIterator = googlePlaySongs.firstElementChild;
 songTableIterator = songTableIterator.nextElementSibling;
 console.log("This is first item in playlist");
@@ -20,13 +20,22 @@ function scraperAndScroller() {
   console.log("This is the songTabelIterator album title: " + songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText);
   console.log("This is the songTableCounter " + songTableCounter);
   songTableCounter++;
-  songScrapArray.push([songTableIterator.childNodes[1].childNodes[1].innerText,songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerText,songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText]);
+  songScrapArray.push(songTableIterator.childNodes[1].childNodes[1].innerText + "," + songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerText+","+songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText);
   songTableIterator = songTableIterator.nextElementSibling;
   songTableIterator.scrollIntoView();
   if (songTableCounter == songTableLength) {
     clearInterval(scraperFunction);
     console.log("This is the songScrapArray");
     console.log(songScrapArray);
+    // Following is test
+    var csvString = songScrapArray.join("%0A");
+    var a         = document.createElement('a');
+    a.href        = 'data:attachment/csv,' + csvString;
+    a.target      = '_blank';
+    a.download    = 'myFile.csv';
+
+    document.body.appendChild(a);
+    a.click();
   }
 }
 
