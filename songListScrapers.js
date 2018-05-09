40e09f1b6f652a783a4cd4c9e328e4136b26539b
chrisnnchris <chrisnnchris@gmail.com>
2018-05-08 18:19:00 -0700

    Google Play Scraper done
    next is export CSV

--- songListScrapers.js
+++ songListScrapers.js
@@ -4,6 +4,32 @@ var songTableLength = googlePlaySongs.getAttribute("data-count");
 console.log(googlePlaySongs);
 console.log("This is songTableLength " + songTableLength); // works, but NOT TESTED ABOVE 50 songs
 var songScrapArray = [];
+
+songTableIterator = googlePlaySongs.firstElementChild;
+songTableIterator = songTableIterator.nextElementSibling;
+console.log("This is first item in playlist");
+console.log(songTableIterator);
+
+songTableCounter = 0;
+var scraperFunction = setInterval(function() { scraperAndScroller() }, 400);
+function scraperAndScroller() {
+  console.log("This is the song table iterator");
+  console.log(songTableIterator);
+  console.log("This is the songTableIterator song title: " + songTableIterator.childNodes[1].childNodes[1].innerText);
+  console.log("This is the songTableIterator artist title: " + songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerText);
+  console.log("This is the songTabelIterator album title: " + songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText);
+  console.log("This is the songTableCounter " + songTableCounter);
+  songTableCounter++;
+  songScrapArray.push([songTableIterator.childNodes[1].childNodes[1].innerText,songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerText,songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText]);
+  songTableIterator = songTableIterator.nextElementSibling;
+  songTableIterator.scrollIntoView();
+  if (songTableCounter == songTableLength) {
+    clearInterval(scraperFunction);
+    console.log("This is the songScrapArray");
+    console.log(songScrapArray);
+  }
+}
+
 /*
 var songTableIterator = googlePlaySongs.firstElementChild;
 console.log("This is songTableIterator");
@@ -18,29 +44,12 @@ console.log(songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerTex
 console.log("This is supposed to be album title ");
 console.log(songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText);
 */
-songTableIterator = googlePlaySongs.firstElementChild;
-songTableIterator = songTableIterator.nextElementSibling;
-console.log("This is first item in playlist");
-console.log(songTableIterator);
 //var delayInMilliseconds = 1000;
 //document.getElementById("mainPanel").scrollIntoView();
 /*
       setTimeout(function() {
       }, delayInMilliseconds)
 */
-songTableCounter = 0;
-var scraperFunction = setInterval(function() { scraperAndScroller() }, 1000);
-function scraperAndScroller() {
-  console.log("This is the song table iterator");
-  console.log(songTableIterator);
-  console.log("This is the songTableCounter " + songTableCounter);
-  songTableCounter++;
-  songTableIterator = songTableIterator.nextElementSibling;
-  songTableIterator.scrollIntoView();
-  if (songTableCounter == songTableLength) {
-    clearInterval(scraperFunction);
-  }
-}
 
 /*
 for (i = 0; i < songTableLength - 1; i++) {
