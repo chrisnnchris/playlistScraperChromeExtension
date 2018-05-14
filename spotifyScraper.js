01ee61b57760127d600b72ffd9e008d21bce7461
chrisnnchris <chrisnnchris@gmail.com>
2018-05-14 14:57:22 -0700

    Started on Spotify Scraper

---
+++ spotifyScraper.js
@@ -0,0 +1,3 @@
+var tracklist = document.getElementsByClassName("tracklist");
+console.log(tracklist[0].children[0].children[0].children[1].children[0].children[0].innerText); // song title
+console.log(tracklist[0].children[0].children[0].children[1].children[0].children[1]); // artist title
