8b85313ae93897d8739a6a900f8010ad0b943b8d
chrisnnchris <chrisnnchris@gmail.com>
2018-05-13 20:32:17 -0700

    Google Play Music Finished
    Next is...spotify?

--- songListScrapers.js
+++ songListScrapers.js
@@ -12,6 +12,7 @@ console.log(songTableIterator);
 songTableCounter = 0;
 var scraperFunction = setInterval(function() { scraperAndScroller() }, 400);
 function scraperAndScroller() {
+  var topElement = document.getElementById("imageWrapper");
   console.log("This is the song table iterator");
   console.log(songTableIterator);
   console.log("This is the songTableIterator song title: " + songTableIterator.childNodes[1].childNodes[1].innerText);
@@ -19,10 +20,10 @@ function scraperAndScroller() {
   console.log("This is the songTabelIterator album title: " + songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText);
   console.log("This is the songTableCounter " + songTableCounter);
   songTableCounter++;
-  var songTitleToAdd = songTableIterator.childNodes[1].childNodes[1].innerText;
+  var songTitleToAdd = "\"" + songTableIterator.childNodes[1].childNodes[1].innerText + "\"";
   //songTitleToAdd = songTitleToAdd.replace(/,/g,"\",\"");
-  var songArtistToAdd = songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerText;
-  var songAlbumToAdd = songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText;
+  var songArtistToAdd = "\"" +  songTableIterator.childNodes[3].childNodes[0].childNodes[0].innerText  + "\"";
+  var songAlbumToAdd = "\"" +  songTableIterator.childNodes[4].childNodes[0].childNodes[0].innerText  + "\"";
   songScrapArray.push(songTitleToAdd + "," + songArtistToAdd +","+ songAlbumToAdd);
   songTableIterator = songTableIterator.nextElementSibling;
   songTableIterator.scrollIntoView();
@@ -32,18 +33,21 @@ function scraperAndScroller() {
     console.log(songScrapArray);
     // Following is test
 
-    var csvString = songScrapArray.join("%0A");
+    var csvString = songScrapArray.join("\n");
     console.log("This is the CSV string");
     console.log(csvString);
-    /*
+    var encodedCSVString = encodeURI(csvString);
+    console.log(encodedCSVString);
+    topElement.scrollIntoView();
+
     var a         = document.createElement('a');
-    a.href        = 'data:attachment/csv,' + csvString;
+    a.href        = 'data:attachment/csv,' + encodedCSVString;
     a.target      = '_blank';
     a.download    = 'myFile.csv';
 
     document.body.appendChild(a);
     a.click();
-    */
+
   }
 }
 
