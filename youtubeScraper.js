dd4151cadaa716748abbc8745ea3a90538eba03f
chrisnnchris <chrisnnchris@gmail.com>
2018-10-01 19:23:44 -0700

    Only excel parse now

--- youtubeScraper.js
+++ youtubeScraper.js
@@ -1,7 +1,10 @@
 console.log("This should output in the main window");
 var youtubeSongs = document.getElementsByClassName("ytd-playlist-video-list-renderer");
+var scrollTopElement = youtubeSongs[0].children[0];
+//console.log(scrollTopElement);
+scrollTopElement.scrollIntoView();
 var totalSongs = document.getElementById("stats");
-console.log(youtubeSongs);
+//console.log(youtubeSongs);
 //console.log("This is total songs");
 //console.log(totalSongs.children[0].innerHTML);
 
@@ -22,26 +25,60 @@ console.log("This is totalSongNumber: " + totalSongNumber);
 var youtubeSongsArr = [];
 
 // for loop that iterates to number of songs
-for (i = 1; i <= 80 ; i++) { // replace 80 with totalSongNumber
-  console.log("i: " + i);
+var scrapingSongs = true;
+var youtubeSongsItr = 1;
+
+
+var scraperInterval = setInterval(function() {
+  youtubeSongs[youtubeSongsItr].scrollIntoView();
+  console.log("This is youtubeSongsItr: " + youtubeSongsItr);
+  console.log(youtubeSongs[youtubeSongsItr].children[2].children[0].children[1].children[0].children[1].innerHTML);
+  youtubeSongsArr.push(youtubeSongs[youtubeSongsItr].children[2].children[0].children[1].children[0].children[1].innerHTML);
+
+  youtubeSongsItr++;
+  if (youtubeSongsItr == parseInt(totalSongNumber+1)) {
+    clearInterval(scraperInterval);
+    for (i = 0; i < youtubeSongsArr.length; i++) {
+      youtubeSongsArr[i] = youtubeSongsArr[i].substring(17, youtubeSongsArr[i].length - 15);
+      //console.log(youtubeSongsArr[i].substring(17, youtubeSongsArr[i].length - 15));
+
+    }
+    console.log("This is youtubeSongsArr after adjustment");
+    console.log(youtubeSongsArr);
+  }
+}, 300)
+/*
+for (i = 1; i <= totalSongNumber; i++) { // replace 80 with totalSongNumber
+  //console.log("i: " + i);
+  youtubeSongs[i].scrollIntoView();
+  //setTimeout(function(){ console.log("waiting"); }, 3000);
+
   console.log(youtubeSongs[i].children[2].children[0].children[1].children[0].children[1].innerHTML);
+
+  if (i == 99) {
+    console.log("scrolling");
+  }
+
   youtubeSongsArr.push(youtubeSongs[i].children[2].children[0].children[1].children[0].children[1].innerHTML);
   // zoom at the 80th mark
-}
 
+}
+*/
 //console.log("This is youtubeSongsArr");
 //console.log(youtubeSongsArr);
 
 //console.log("outputting song titles without spaces");
+/*
 for (i = 0; i < youtubeSongsArr.length; i++) {
   youtubeSongsArr[i] = youtubeSongsArr[i].substring(17, youtubeSongsArr[i].length - 15);
   //console.log(youtubeSongsArr[i].substring(17, youtubeSongsArr[i].length - 15));
 
 }
-
+*/
+/*
 console.log("This is youtubeSongsArr after adjustment");
 console.log(youtubeSongsArr);
-
+*/
 
 //'↵                ' // first part, 17
 //'↵              ' // second part , 15
